006a1b9f6581609e3a3593d552662aa845667d18
DOF1109 <friasdaniel.1109@gmail.com>
2024-03-21 09:21:27 -0300

    Rutas completas

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import Form from "./components/pages/Form/Form";
+import Home from "./components/pages/Home/Home";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/pages/Login/Login";
+
 function App() {
   return (
-    <>
-      <h1>App</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/form" element={<Form />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
