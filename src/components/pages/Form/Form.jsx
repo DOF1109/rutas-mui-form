818855dd67794df71a33b82fec4cd329ff806c72
DOF1109 <friasdaniel.1109@gmail.com>
2024-03-21 11:46:33 -0300

    Se completa validacion con YUP

--- src/components/pages/Form/Form.jsx
+++ src/components/pages/Form/Form.jsx
@@ -1,17 +1,33 @@
 import { Button, Grid, TextField, Typography } from "@mui/material"
 import "./Form.css"
-import { useState } from "react"
+import { useFormik } from "formik"
+import { object, string } from "yup"
 
 const Form = () => {
-    const [name, setName] = useState()
     
+    const formik = useFormik({
+        initialValues: {
+            name: "",
+            email: "",
+            password: ""
+        },
+        validationSchema: object({
+            name: string().required("Enter a name"),
+            email: string().email().required("Enter a email"),
+            password: string().required("Enter a password")
+        }),
+        onSubmit: (data) => {
+            console.log(data);
+        }
+    })
+
     return(
         <div>
             <Typography color="primary" variant="h2" align="center" m={4}>
                 Register form
             </Typography>
 
-            <form className="form-container">
+            <form className="form-container" onSubmit={ formik.handleSubmit }>
                 <Grid 
                     container 
                     alignItems="center" 
@@ -26,7 +42,11 @@ const Form = () => {
                             label="Name" 
                             variant="outlined"
                             fullWidth
-                            onChange={ e => setName(e.target.value) } />
+                            name="name"
+                            onChange={formik.handleChange}
+                            value={formik.values.name}
+                            error={formik.errors.name}
+                            helperText={formik.errors.name} />
                     </Grid>
 
                     <Grid item xs={12} md={7}>
@@ -34,7 +54,12 @@ const Form = () => {
                             type="email" 
                             label="Email" 
                             variant="outlined"
-                            fullWidth />
+                            fullWidth
+                            name="email"
+                            onChange={formik.handleChange}
+                            value={formik.values.email}
+                            error={formik.errors.email}
+                            helperText={formik.errors.email} />
                     </Grid>
 
                     <Grid item xs={12} md={7}>
@@ -42,7 +67,12 @@ const Form = () => {
                             type="password" 
                             label="Password" 
                             variant="outlined"
-                            fullWidth />
+                            fullWidth
+                            name="password"
+                            onChange={formik.handleChange}
+                            value={formik.values.password}
+                            error={formik.errors.password}
+                            helperText={formik.errors.password} />
                     </Grid>
 
                 </Grid>
